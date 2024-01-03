9523e78183fecc673c6e81fd65b51035bc126cad
MAMCB <96302464+MAMCB@users.noreply.github.com>
2024-01-03 11:55:23 +0100

    created get all users route

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT ||8000;
+const {Pool} = require("pg");
+require("dotenv/config");
+
+const pool = new Pool({
+    connectionString:process.env.ELEPHANT_SQL_CONNECTION_STRING,
+});
 
 
-app.get("/",(req,res)=>res.send("Hello world!"))
+app.get("/",(req,res)=>res.send("Hello world!"));
+
+app.get("/api/users", (req,res)=>{
+    pool.query("SELECT * FROM users;").then(data=>{
+        res.json(data.rows);
+    }).catch(e=>{
+        res.status(500).json({message:e.message});
+    })
+});
 
 
 
